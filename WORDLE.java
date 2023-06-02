44b27edc6aa03d0e7a034390691a6e2d56abbb2e
Deepak-183 <134669155+Deepak-183@users.noreply.github.com>
2023-06-02 13:56:03 +0530

    Update WORDLE.java

--- WORDLE.java
+++ WORDLE.java
@@ -5,20 +5,24 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Codechef
+class Wordle
 {	public static void main (String[] args) throws java.lang.Exception
-	{	    	Scanner s = new Scanner(System.in);
-	  int t = s.nextInt();
-	  while(t-->0){
-	   	String S = s.next();
-	String R = s.next();
-	String m[]=new String[5];
-	for(int i=0;i<5;i++){
-	if(S.charAt(i)==(R.charAt(i))) m[i]="G";
-	else m[i]="B";
-	System.out.print(m[i]);
-	}
-		System.out.print("\n");
-	}   
+	{	    	
+		Scanner s = new Scanner(System.in);
+	  	int t = s.nextInt();
+	  	while(t-->0)
+		{
+	   		String S = s.next();
+			String R = s.next();
+			String m[]=new String[5];
+			for(int i=0;i<5;i++){
+				if(S.charAt(i)==(R.charAt(i))) 
+					m[i]="G";
+				else 
+					m[i]="B";
+					System.out.print(m[i]);
+			}
+			System.out.print("\n");
+		}   
 	  }
 }
